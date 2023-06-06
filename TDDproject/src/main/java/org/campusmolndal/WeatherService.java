fa85aa9c2d34677b2ed2f7b70fcd63d95b4089e1
Fredrik <91frerin@gafe.molndal.se>
2023-06-06 14:28:22 +0200

    gjort en testklass m.m.

---
+++ TDDproject/src/main/java/org/campusmolndal/WeatherService.java
@@ -0,0 +1,6 @@
+package org.campusmolndal;
+
+public class WeatherService {
+
+
+}
---
+++ TDDproject/src/test/java/org/campusmolndal/demo/WeatherServiceTest.java
@@ -0,0 +1,12 @@
+package org.campusmolndal.demo;
+
+import org.campusmolndal.WeatherService;
+import org.junit.jupiter.api.BeforeEach;
+
+public class WeatherServiceTest {
+
+@BeforeEach
+void setUp() {
+    WeatherService weatherService = new WeatherService();
+}
+}
